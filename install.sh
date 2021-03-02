1d8fe42b46b88ed52c6355d832721178672ed002
Jeffrey Bagdis <9042243+jbagdis@users.noreply.github.com>
2021-03-01 22:48:57 -0500

    add thefuck

--- install.sh
+++ install.sh
@@ -5,4 +5,7 @@ pushd ${0:a:h}
 if [ $SPIN ]; then
   ln -sf `pwd`/gitconfig "$HOME"/.gitconfig
   ln -sf `pwd`/gitignore_global "$HOME"/.gitignore_global
+  
+  sudo apt-get install thefuck
+  sudo ln -s thefuck /bin/fuck
 fi
