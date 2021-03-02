cb0d58553090a335087807107059b5c64b490d83
Jeffrey Bagdis <9042243+jbagdis@users.noreply.github.com>
2021-03-01 21:25:00 -0500

    Use zsh and switch to script directory

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/env zsh
 
-ls -sf gitconfig "$HOME"/.gitconfig
-ls -sf gitignore_global "$HOME"/.gitignore_global
+pushd ${0:a:h}
+pwd
+
+if [ $SPIN ]; then
+  ls -sf gitconfig "$HOME"/.gitconfig
+  ls -sf gitignore_global "$HOME"/.gitignore_global
+fi
