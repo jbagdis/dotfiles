6b0cde91dc194073cb08bd9383c139dc631bd802
Jeffrey Bagdis <9042243+jbagdis@users.noreply.github.com>
2021-03-01 21:26:04 -0500

    use ln

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ pushd ${0:a:h}
 pwd
 
 if [ $SPIN ]; then
-  ls -sf gitconfig "$HOME"/.gitconfig
-  ls -sf gitignore_global "$HOME"/.gitignore_global
+  ln -sf gitconfig "$HOME"/.gitconfig
+  ln -sf gitignore_global "$HOME"/.gitignore_global
 fi
