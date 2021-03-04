427328ebea4990e18154afd25c1e4f3f4e7bd65e
Jeffrey Bagdis <9042243+jbagdis@users.noreply.github.com>
2021-03-04 00:06:39 -0500

    add a tmux config

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ pushd ${0:a:h}
 if [ $SPIN ]; then
   ln -sf `pwd`/gitconfig "$HOME"/.gitconfig
   ln -sf `pwd`/gitignore_global "$HOME"/.gitignore_global
+  ln -sf `pwd`/tmux.conf "$HOME"/.tmux.conf
+  tmux source-file ~/.tmux.conf
 fi
